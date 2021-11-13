f102ec8baaf6e9f4224f7b343c39f62880c70740
qhhivan <orsolic.i02@htlwienwest.at>
2021-11-13 14:31:51 +0100

    getEvents v2

--- server/app.js
+++ server/app.js
@@ -3,4 +3,30 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const path = require('path');
 // Routes
-const routes = require('./r')
+const routes = require('./routes/kaffeehaus');
+// Colors und so einbinden
+require('colors');
+require('dotenv').config();
+
+// Middleware
+const { errorHandler, notFoundHandler } = require('./middleware/errorHandler');
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(helmet());
+
+app.use(express.json());
+
+// routes
+app.use('/', routes);
+
+app.use(notFoundHandler);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT ?? 5000;
+
+app.listen(PORT);
